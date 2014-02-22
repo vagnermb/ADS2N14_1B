fe079ecd421d0962ac1fde358c7e267afe1a2f94
Vágner Borges <vagnerborges@hotmail.com.br>
2014-02-22 20:09:00 -0300

    Modificação Hello You.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,14 @@
-public class Hello {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        out.printIn(null, "Hello World!");
-        System.exit(0);
-    }
+public class Ler{
+    public static void main (String [] args){
+   
+   Scanner entrada = new Scanner(System.in); 
+   String nome ="";
+   System.out.println("Digite o seu nome.");
+   nome = entrada.next();
+   System.out.println("Hello"+nome);
+
+   }
 }
 
